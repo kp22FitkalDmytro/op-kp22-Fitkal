10f635ebded2da404868365e9e63f470c9b6800a
kp22FitkalDmytro <117391309+kp22FitkalDmytro@users.noreply.github.com>
2023-03-28 11:14:58 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // читає слова з файліка
+        string[] words = File.ReadAllLines("input.txt");
+
+        // сортує за алфавітом
+        Array.Sort(words);
+
+        // виписує просортовані числа в файлік
+        File.WriteAllLines("output.txt", words);
+
+        Console.WriteLine("Слова просортовані та написані в output.txt:");
+        Console.WriteLine(string.Join(", ", words));
+    }
+}
