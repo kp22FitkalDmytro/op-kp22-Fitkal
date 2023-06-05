f071f9eb141cf9ef4a3171fe5618d4f591e351ee
kp22FitkalDmytro <117391309+kp22FitkalDmytro@users.noreply.github.com>
2023-06-05 14:30:15 +0300

    Add files via upload

---
+++ InternationalFlight.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class InternationalFlight : Flight
+{
+    List<string> surnames = new List<string>() { "Surname1", "Surname2", "Surname3" };
+    List<int> visaNumber = new List<int>() { 121, 131, 141, 151};
+    public InternationalFlight()
+    {
+    }
+    public InternationalFlight(int fn, int rn, string dt, string at, string dp, string ap, string ip)
+    {
+        flightNumber = fn;
+        raceNumber = rn;
+        depatureTime = dt;
+        arrivalTime = at;
+        depaturePlace = dp;
+        arrivalPlace = ap;
+        intermediatePoint = ip;
+    }
+    public override void Check()
+    {
+        Client client = new Client();
+        Console.WriteLine("Enter your surname");
+        client.SurName = Console.ReadLine();
+        Console.WriteLine("Enter your visa number");
+        client.VisaNumber = int.Parse(Console.ReadLine());
+        for (int i = 0; i < surnames.Count; i++)
+        {
+            for(int j = 0; j < visaNumber.Count; j++)
+            {
+                if (surnames[i] == client.SurName && visaNumber[i]==client.VisaNumber)
+                {
+                    Console.WriteLine("You are eligable");
+                }
+                else
+                {
+                    Console.WriteLine("You are not eligable");
+                }
+            }
+
+        }
+    }
+}
+
