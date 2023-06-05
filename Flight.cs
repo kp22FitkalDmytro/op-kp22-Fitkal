e5babddc83c356aa963b6072b749c17d35376dd3
kp22FitkalDmytro <117391309+kp22FitkalDmytro@users.noreply.github.com>
2023-06-05 14:30:41 +0300

    Add files via upload

---
+++ Flight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+abstract class Flight
+{
+    public int flightNumber { get; set; }
+    public string depatureTime { get; set; }
+    public string arrivalTime { get; set; }
+    public int raceNumber { get; set; }
+    public string intermediatePoint { get; set; }
+    public string depaturePlace { get; set; }
+    public string arrivalPlace { get; set; }
+    public void PrintMethod()
+    {
+        Console.WriteLine($"{flightNumber}) The race number: {raceNumber}, " +
+            $"depature time: {depatureTime}, " +
+            $" arrival time: {arrivalTime}, " +
+            $"depature place: {depaturePlace}, " +
+            $"arrivall place: {arrivalPlace}, " +
+            $"intermediate point: {intermediatePoint}");
+        Console.WriteLine("");
+    }
+    public abstract void Check();
+}
+
